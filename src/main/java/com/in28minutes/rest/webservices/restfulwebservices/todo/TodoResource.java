6c040bd7305f2e9d23fb46773d6fb0be80d9f263
sunwooiljoon <wnsdl1659@naver.com>
2023-10-15 20:00:12 +0900

    Initial commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -0,0 +1,50 @@
+package com.in28minutes.rest.webservices.restfulwebservices.todo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TodoResource {
+    private TodoService todoService;
+
+    private TodoRepository todoRepository;
+
+    public TodoResource(TodoService todoService, TodoRepository todoRepository) {
+        this.todoService = todoService;
+        this.todoRepository = todoRepository;
+    }
+
+    @GetMapping("/users/{username}/todos")
+    public List<Todo> retrieveTodos(@PathVariable String username) {
+        return todoRepository.findByUsername(username);
+    }
+
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo retrieveTodo(@PathVariable String username, @PathVariable int id) {
+        return todoRepository.findById(id).get();
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
+        todoRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/users/{username}/todos/{id}")
+    public Todo updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo) {
+        todoRepository.save(todo);
+        return todo;
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public Todo createTodo(@PathVariable String username, @RequestBody Todo todo) {
+
+        todo.setUsername(username);
+        todo.setId(null);
+
+        return todoRepository.save(todo);
+    }
+}
